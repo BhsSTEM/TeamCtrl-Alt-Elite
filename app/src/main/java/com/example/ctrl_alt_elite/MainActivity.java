ebb68d98e3c69c311f3431d57216616ad2b235d8
alexlarsen33 <alexflarsen@gmail.com>
2026-02-26 15:19:46 -0600

    Added a comment with my name (alex).

--- app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
+++ app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+//Hello I am alex hi hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
