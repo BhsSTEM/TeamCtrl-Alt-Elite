0e1c8b0bb8f1f0b4328fa770d0b2f1e132bf6e26
alexlarsen33 <alexflarsen@gmail.com>
2026-03-10 15:11:58 -0500

    Remove a few errors

--- app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
+++ app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
