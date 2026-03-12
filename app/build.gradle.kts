da941cec6ef96af9140e808580c990073964b208
EvanBaker0 <evanjbaker264@gmail.com>
2026-03-12 15:11:49 -0500

    comit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -42,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,17 @@ plugins {
 
 
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         // ...
+
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
