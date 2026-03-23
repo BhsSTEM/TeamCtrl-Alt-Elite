0cb98b1ee05002b4c22d31c97165d2018a8da22f
rjett149309 <rose149309@gmail.com>
2026-03-23 14:52:08 -0500

    random 03/23

--- app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
+++ app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
@@ -3,22 +3,22 @@ package com.example.ctrl_alt_elite;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
+//Hello I am alex hi hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        // This puts activity_main INSIDE activity_base's FrameLayout
-        setActivityContent(R.layout.activity_main);
-
-        // Highlight the Home icon
-        BottomNavigationView nav = findViewById(R.id.bottom_nav);
-        if (nav != null) {
-           nav.setSelectedItemId(R.id.nav_home);
-        }
+        setContentView(R.layout.activity_main); // LINE THAT changes how the page is set up
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
