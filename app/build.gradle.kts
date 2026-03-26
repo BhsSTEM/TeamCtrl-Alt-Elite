c5517b9039ba878a72c0ff7161b8b0fd3a2095d3
AbbyR14 <abbyrashid4@gmail.com>
2026-03-26 15:17:12 -0500

    The weatha is coming 3/26/2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    
+    // Weather API dependencies
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.glide)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
