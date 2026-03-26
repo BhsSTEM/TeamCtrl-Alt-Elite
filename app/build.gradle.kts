260677086901f862b37505c2433740ee289fd7f6
rjett149309 <rose149309@gmail.com>
2026-03-26 15:17:41 -0500

    added working buttons on loginPage added signInPage added firebase compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,13 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
     implementation(libs.play.services.maps)
     implementation("com.google.android.gms:play-services-location:21.3.0")
+    // Import the BoM (Bill of Materials) for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 }
