8e7f0163cc22cfdb92524ed5ae4d0a695a62f4ec
AbbyR14 <abbyrashid4@gmail.com>
2026-04-07 15:06:46 -0500

    Map on MainActivity takes you to evansMapPage + fits BaseActivity 4/7/2026

--- app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
+++ app/src/main/java/com/example/ctrl_alt_elite/MainActivity.java
@@ -48,11 +48,7 @@ public class MainActivity extends BaseActivity {
         EdgeToEdge.enable(this);
         setActivityContent(R.layout.activity_main);
         
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
 
         // Initialize Weather Views with your updated XML IDs
         tempText = findViewById(R.id.weather_temp);
