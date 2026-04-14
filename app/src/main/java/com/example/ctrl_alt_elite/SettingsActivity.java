78aa663eb225f0c45c57ad64c0404de4b4afa98f
rjett149309 <rose149309@gmail.com>
2026-04-14 14:47:24 -0500

    Made logout button work on SettingsActivity

--- app/src/main/java/com/example/ctrl_alt_elite/SettingsActivity.java
+++ app/src/main/java/com/example/ctrl_alt_elite/SettingsActivity.java
@@ -1,8 +1,23 @@
 package com.example.ctrl_alt_elite;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+
+
 
 public class SettingsActivity extends BaseActivity {
+
+    private FirebaseAuth mAuth;
+    private Button logoutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -10,5 +25,15 @@ public class SettingsActivity extends BaseActivity {
         // This puts activity_main INSIDE activity_base's FrameLayout
         setActivityContent(R.layout.activity_settings);
 
+        mAuth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.signOutButton);
+//makes logout button work
+logoutButton.setOnClickListener(View -> {
+    mAuth.signOut();
+    startActivity(new Intent(SettingsActivity.this, LoginPage.class));
+    finish();
+});
     }
+
+
 }
