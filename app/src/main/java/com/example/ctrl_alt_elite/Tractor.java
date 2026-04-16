8f06f974ced77739d459091832712b7b88b4e082
EvanBaker0 <evanjbaker264@gmail.com>
2026-04-16 15:30:50 -0500

    added database stuff on machines page showing joemamas stuff

--- app/src/main/java/com/example/ctrl_alt_elite/Tractor.java
+++ app/src/main/java/com/example/ctrl_alt_elite/Tractor.java
@@ -1,17 +1,46 @@
 package com.example.ctrl_alt_elite;
+import com.google.firebase.firestore.PropertyName;
 
-public class Tractor {
-    private String name;
-    private String year;
-    private String model;
+    public class Tractor {
+        private String name;
+        private String model;
+        private int year;
+        private String status;
+        private int fuel;
+        private String lastUpdated;
+        private String pin;
 
-    public Tractor(String name, String year, String model) {
-        this.name = name;
-        this.year = year;
-        this.model = model;
-    }
+        private String imageUrl;
 
-    public String getName() { return name; }
-    public String getYear() { return year; }
-    public String getModel() { return model; }
+        public Tractor() {}
+//data
+        @com.google.firebase.firestore.PropertyName("tracterName")
+        public String getName() { return name; }
+        @com.google.firebase.firestore.PropertyName("tracterName")
+        public void setName(String name) { this.name = name; }
+
+        @com.google.firebase.firestore.PropertyName("modelNumber")
+        public String getModel() { return model; }
+        @com.google.firebase.firestore.PropertyName("modelNumber")
+        public void setModel(String model) { this.model = model; }
+
+        @com.google.firebase.firestore.PropertyName("imageUrl")
+        public String getImageUrl() { return imageUrl; }
+        @com.google.firebase.firestore.PropertyName("imageUrl")
+        public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+        public String getStatus() { return status; }
+        public void setStatus(String status) { this.status = status; }
+        public int getFuel() { return fuel; }
+        public void setFuel(int fuel) { this.fuel = fuel; }
+        public String getLastUpdated() { return lastUpdated; }
+        public void setLastUpdated(String lastUpdated) { this.lastUpdated = lastUpdated; }
+        public int getYear() { return year; }
+        public void setYear(int year) { this.year = year; }
+        public String getPin() { return pin; }
+        public void setPin(String pin) { this.pin = pin; }
+
 }
+
+
+
+/////
